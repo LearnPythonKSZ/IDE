e3fb917324547116245b44d851a204b8a279be41
LearnPythonKSZ <33370042+LearnPythonKSZ@users.noreply.github.com>
2018-01-18 21:23:28 +0100

    Update script.js

--- CSS_JSS/script.js
+++ CSS_JSS/script.js
@@ -15,6 +15,27 @@ function number(){
 		document.getElementById("unlocked").innerHTML="Du hast das nächste Kapitel freigeschaltet";
 	}
 }
+function solution(idbutton){
+	if (idbutton.getAttribute("id")=="solutionbutton1"){
+		var solutiontag=document.getElementById("solutiontag1");
+		var solutionbutton=document.getElementById("solutionbutton1");
+		var solutionspan=document.getElementById("solution1");
+		solutionbutton.style.width = "800px";
+		solutionbutton.style.height = "400px";
+		solutionbutton.style.left="calc(50% - 400px)";
+		solutiontag.style.display="none";
+		solutionspan.style.display="inline";
+	}else if(idbutton.getAttribute("id")=="solutionbutton2"){
+		var solutiontag=document.getElementById("solutiontag2");
+		var solutionbutton=document.getElementById("solutionbutton2");
+		var solutionspan=document.getElementById("solution2");
+		solutionbutton.style.width = "800px";
+		solutionbutton.style.height = "400px";
+		solutionbutton.style.left="calc(50% - 400px)";
+		solutiontag.style.display="none"
+		solutionspan.style.display="inline";
+	}
+}
 function help(idtext,idbutton,scroll){
 	var idh = idbutton.getAttribute("id");
 	if (idh == "helpbutton1") {
