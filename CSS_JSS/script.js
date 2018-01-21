59ea22a114f21d8c668aa2396644ee734328f05c
LearnPythonKSZ <33370042+LearnPythonKSZ@users.noreply.github.com>
2018-01-21 14:05:54 +0100

    Update script.js

--- CSS_JSS/script.js
+++ CSS_JSS/script.js
@@ -48,7 +48,9 @@ function solution(idbutton){
  }
  function help(idtext,idbutton,scroll){
 	var idButtonElement = document.getElementById("idbutton");
+	window.alert(idButtonElement);
 	var idh = idButtonElement.getAttribute("id");
+	window.alert(idh);
  	if (idh == "helpbutton1") {
  	idbutton.style.display = "none" ;
  	idtext.style.display = "block";
